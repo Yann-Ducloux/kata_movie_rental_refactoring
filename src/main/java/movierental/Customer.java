6475b00ad3e478a983fea471a5791a9237b7201f
Yann DUCLOUX <yann.ducloux@lacombedulionvert.fr>
2023-03-16 10:09:14 +0100

    refactoring: rename attribut

--- src/main/java/movierental/Customer.java
+++ src/main/java/movierental/Customer.java
@@ -5,19 +5,19 @@ import java.util.List;
 
 public class Customer {
 
-    private String _name;
-    private List<Rental> _rentals = new ArrayList<Rental>();
+    private String name;
+    private List<Rental> rentals = new ArrayList<Rental>();
 
     public Customer(String name) {
-        _name = name;
+        this.name = name;
     }
 
     public void addRental(Rental arg) {
-        _rentals.add(arg);
+        rentals.add(arg);
     }
 
     public String getName() {
-        return _name;
+        return name;
     }
 
     public String statement() {
@@ -25,32 +25,34 @@ public class Customer {
         int frequentRenterPoints = 0;
         String result = "Rental Record for " + getName() + "\n";
 
-        for (Rental each : _rentals) {
+        for (Rental each : rentals) {
             double thisAmount = 0;
 
             //determine amounts for each line
             switch (each.getMovie().getPriceCode()) {
                 case Movie.REGULAR:
                     thisAmount += 2;
-                    if (each.getDaysRented() > 2)
+                    if (each.getDaysRented() > 2) {
                         thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    }
                     break;
                 case Movie.NEW_RELEASE:
                     thisAmount += each.getDaysRented() * 3;
                     break;
                 case Movie.CHILDRENS:
                     thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
+                    if (each.getDaysRented() > 3) {
                         thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    }
                     break;
             }
 
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
                 frequentRenterPoints++;
-
+            }
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
